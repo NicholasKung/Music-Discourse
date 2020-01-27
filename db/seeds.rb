b3f655b0626feb0f7a23fc2f93fe9fcbfec83e96
Zain Jakhar <zainjakhar@gmail.com>
2020-01-27 16:00:08 -0500

    seed dummy data for test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Album.create(album:"Rare", art:"https://www.udiscovermusic.com/wp-content/uploads/2019/12/Selena-Gomez-Rare-album-cover-820.jpg", artist: "Selena Gomez", year: "2020", genre: "pop")
