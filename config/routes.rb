91a6b1357dfbf3254da92c7f2bcfd04405b086c2
Zain Jakhar <zainjakhar@gmail.com>
2020-01-27 15:09:13 -0500

    added route for api endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   get "/albums", to: 'homes#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :albums, only [:index]
+    end
+  end
+
 end
