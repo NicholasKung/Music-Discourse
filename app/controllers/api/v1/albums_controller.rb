98bad764b951d2164ca8078f04b45de4cc002e9e
Zain Jakhar <zainjakhar@gmail.com>
2020-01-27 17:19:41 -0500

    removed security

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::AlbumsController < ApplicationController
-  protect_from_forgery unless: -> { request.format.json? }
   def index
-    render json: Album.all 
+    render json: Album.all
   end
 end
