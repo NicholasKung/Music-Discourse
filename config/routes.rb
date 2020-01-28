3c9e9231ac629e1ce923c93f77592a55a3b0d124
Evan Fernberg <evan@Evans-MacBook-Pro.local>
2020-01-28 17:13:36 -0500

    added routes for show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   get "/albums", to: 'homes#index'
+  get "/albums/:id", to: 'homes#index'
 
   namespace :api do
     namespace :v1 do
-      resources :albums, only: [:index]
+      resources :albums, only: [:index, :show]
     end
   end
-
 end
