54d831588d994daf00e5dfb6162efe1b26348825
Zain Jakhar <zainjakhar@gmail.com>
2020-01-29 16:12:29 -0500

    created controller for Create and sanatized album form input

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -1,5 +1,22 @@
 class Api::V1::AlbumsController < ApplicationController
+  protect_from_forgery unless: -> { request.format.json? }
+
   def index
     render json: Album.all
+  end
+
+  def create
+    album = Album.new(album_params)
+    if album.save
+      render json: { album: album }
+    else
+      render json: { error: album.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+private
+
+  def album_params
+    params.permit(:album, :artist, :genre, :year, :art)
   end
 end
