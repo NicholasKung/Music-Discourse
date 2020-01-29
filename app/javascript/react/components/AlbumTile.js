17ff0e95b3f6b3af55edc0cd4f492b7fd1f952b9
Evan Fernberg <evan@Evans-MacBook-Pro.local>
2020-01-29 13:31:28 -0500

    We refactored AlbumTile. Destructured props from entire "album" object and refactored props.

--- app/javascript/react/components/AlbumTile.js
+++ app/javascript/react/components/AlbumTile.js
@@ -1,17 +1,19 @@
 import React, { useState } from "react"
 import { Link } from "react-router-dom"
 
-const AlbumTile = (props) => {
+const AlbumTile = ({ albumData }) => {
+  let { id, album, art, artist, year, genre } = albumData
   
   return(
     <li>
       <div className="show-link">
-        <Link to={`/albums/${props.id}`}><img src={props.art} /></Link>
+        <Link to={`/albums/${id}`}><img src={art} /></Link>
          <br />
       </div>
-        <span>{props.artist}</span> - <p>{props.album}</p>
+        <span>{artist}</span> - <p>{album}</p>
     </li>
   )
 }
+
 
 export default AlbumTile
