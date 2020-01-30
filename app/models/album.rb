228ed9d2429cf6a45280df246ab203db0f20d60c
Zain Jakhar <zainjakhar@gmail.com>
2020-01-30 09:50:10 -0500

    added user association to Album model

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,8 +1,9 @@
 class Album < ApplicationRecord
+  belongs_to :user
+
   validates :album, presence: true
   validates :art, presence: true
   validates :artist, presence: true
   validates :year, numericality: true
   validates :genre, presence: true
-
 end
