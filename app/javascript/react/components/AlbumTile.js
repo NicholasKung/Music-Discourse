9e16c2e0a4aca61cb053e04761dc97dd2bbae691
vinhvinhvroom <vinh.doan13@gmail.com>
2020-01-31 16:06:42 -0500

    took out unecessary useState

--- app/javascript/react/components/AlbumTile.js
+++ app/javascript/react/components/AlbumTile.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react"
+import React from "react"
 import { Link } from "react-router-dom"
 
 const AlbumTile = ({ albumData }) => {
   let { id, album, art, artist, year, genre } = albumData
-  
+
   return(
     <li>
       <div className="show-link">
