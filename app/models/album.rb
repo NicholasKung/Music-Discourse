5a94422d8ed76b981c7876f37e4a8e3fa9b41298
Zain Jakhar <zainjakhar@gmail.com>
2020-01-31 16:32:32 -0500

    added length validation for year

--- app/models/album.rb
+++ app/models/album.rb
@@ -4,6 +4,6 @@ class Album < ApplicationRecord
   validates :album, presence: true
   validates :art, presence: true
   validates :artist, presence: true
-  validates :year, numericality: true
+  validates :year, numericality: true, length: { is: 4 }
   validates :genre, presence: true
 end
