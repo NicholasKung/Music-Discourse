bf2466fc1518cf0bcbe6861e2d8c6613cf033087
vinhvinhvroom <vinh.doan13@gmail.com>
2020-01-31 16:03:36 -0500

    changed require_tree

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= //= require foundation
-require_tree .
+//= require foundation
+//= require_tree .
+
 
 $(function(){ $(document).foundation(); });
