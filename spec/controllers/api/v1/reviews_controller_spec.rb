addd482ce7ffe57b9c96f209d9cdbf6bf2160955
vinhvinhvroom <vinh.doan13@gmail.com>
2020-02-03 13:45:45 -0500

    Took out ids in test instances

--- spec/controllers/api/v1/reviews_controller_spec.rb
+++ spec/controllers/api/v1/reviews_controller_spec.rb
@@ -2,21 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::ReviewsController, type: :controller do
   let!(:user1) {User.create(
-    id: 1,
     email: "yo@yo.com",
     password: "password1",
     role: "member"
     )}
 
   let!(:user2) {User.create(
-    id: 2,
     email: "no@yo.com",
     password: "password2",
     role: "member"
       )}
 
   let!(:album1) {Album.create(
-    id: 1,
     album: "test album 1",
     art: "https://vignette.wikia.nocookie.net/selenagomez/images/4/46/Rare_Album_Cover.jpg/revision/latest?cb=20191212172712",
     artist: "Selena Gomez",
@@ -26,7 +23,6 @@ RSpec.describe Api::V1::ReviewsController, type: :controller do
     )}
 
   let!(:review1) {Review.create(
-    id: 1,
     user_id: 1,
     user: user1,
     album_id: 1,
@@ -36,7 +32,6 @@ RSpec.describe Api::V1::ReviewsController, type: :controller do
   )}
 
   let!(:review2) {Review.create(
-    id: 2,
     user_id: 2,
     user: user2,
     album_id: 1,
@@ -57,7 +52,7 @@ RSpec.describe Api::V1::ReviewsController, type: :controller do
       expect(response.content_type).to eq ("application/json")
 
       expect(returned_json["reviews"][0]["user"]["email"]).to eq "yo@yo.com"
-      expect(returned_json["reviews"][0]["album_id"]).to eq 1
+      expect(returned_json["reviews"][0]["album_id"]).to eq album1.id
       expect(returned_json["reviews"][0]["rating"]).to eq 5
       expect(returned_json["reviews"][0]["review"]).to eq "Decent"
 
@@ -73,7 +68,7 @@ RSpec.describe Api::V1::ReviewsController, type: :controller do
       expect(response.content_type).to eq ("application/json")
 
       expect(returned_json["reviews"][1]["user"]["email"]).to eq "no@yo.com"
-      expect(returned_json["reviews"][1]["album_id"]).to eq 1
+      expect(returned_json["reviews"][1]["album_id"]).to eq album1.id
       expect(returned_json["reviews"][1]["rating"]).to eq 7
       expect(returned_json["reviews"][1]["review"]).to eq "Great"
     end
@@ -82,7 +77,6 @@ RSpec.describe Api::V1::ReviewsController, type: :controller do
   describe "POST#create" do
     context 'when a successful request is made with proper params' do
       let!(:new_review_hash) {{
-        id: 12,
         user: user3,
         user_id: user3.id,
         album: album1,
@@ -92,7 +86,6 @@ RSpec.describe Api::V1::ReviewsController, type: :controller do
       }}
 
       let!(:user3) {User.create(
-        id: 3,
         email: "noyo@yo.com",
         password: "password3"
       )}
