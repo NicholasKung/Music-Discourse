33ec0afabf13fedb56b67659ec1ea19130c2864b
vinhvinhvroom <vinh.doan13@gmail.com>
2020-02-04 15:12:36 -0500

    updated review serializer

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,14 +1,12 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :rating, :review, :user_id, :album_id, :email, :album
+  attributes :id, :rating, :review, :user_id, :album_id, :email, :votes
 
   belongs_to :user
   belongs_to :album
+  has_many :votes
 
   def email
     object.user.email
   end
 
-  def album
-    object.album
-  end
 end
