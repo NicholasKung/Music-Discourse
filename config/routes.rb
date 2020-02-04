8b021e5730a7fae63bdb943db7d3849a53a2a0a4
vinhvinhvroom <vinh.doan13@gmail.com>
2020-02-04 15:12:48 -0500

    added route for votes api

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get "/albums", to: 'homes#index'
   get "/albums/:id", to: 'homes#index'
   get "/albums/new", to: 'homes#index'
+  get "/votes", to: 'homes#index'
 
   namespace :api do
     namespace :v1 do
@@ -13,4 +14,11 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  namespace :api do
+    namespace :v1 do
+      resources :votes, only: [:show, :create]
+    end
+  end
+
 end
