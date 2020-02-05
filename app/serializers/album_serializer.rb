2015b3ed717fcd63aeeb80350ed26908aa7dafb4
Zain Jakhar <zainjakhar@gmail.com>
2020-02-05 09:18:48 -0500

    chose this merge

--- app/serializers/album_serializer.rb
+++ app/serializers/album_serializer.rb
@@ -2,8 +2,5 @@ class AlbumSerializer < ActiveModel::Serializer
   attributes :id, :album, :art, :artist, :year, :genre, :reviews, :user
 
   has_many :reviews
-<<<<<<< HEAD
   belongs_to :user
-=======
->>>>>>> 05ab9dcfcd6d7641aa5cf3e02ef01fd922199338
 end
