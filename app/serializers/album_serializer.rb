51211259494645a15614223cd537581fe22aaf5d
vinhvinhvroom <vinh.doan13@gmail.com>
2020-02-05 14:51:57 -0500

    took out votes in serializer

--- app/serializers/album_serializer.rb
+++ app/serializers/album_serializer.rb
@@ -1,5 +1,20 @@
 class AlbumSerializer < ActiveModel::Serializer
-  attributes :id, :album, :art, :artist, :year, :genre, :reviews
+  attributes :id, :album, :art, :artist, :year, :genre, :reviews, :average
 
   has_many :reviews
+
+  def average
+    review_total = []
+    object.reviews.each do |review|
+      review_total << review.rating
+    end
+    if review_total.length > 0
+      review_average = review_total.sum.to_f / review_total.length
+      return "#{review_average.round(1)}/10"
+    else
+      return "Not Yet Rated"
+    end
+
+  end
+
 end
