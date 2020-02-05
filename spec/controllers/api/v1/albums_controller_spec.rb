3617b461a351720bf82063c5ebfc577a69c64ea7
Evan Fernberg <evan@Evans-MacBook-Pro.local>
2020-02-05 14:28:57 -0500

    added test to api spec

--- spec/controllers/api/v1/albums_controller_spec.rb
+++ spec/controllers/api/v1/albums_controller_spec.rb
@@ -143,4 +143,28 @@ RSpec.describe Api::V1::AlbumsController, type: :controller do
       end
     end
   end
+
+  describe "DELETE#destroy" do
+    context 'When a user clicks the delete button' do
+      let!(:user2) {User.create(
+        email: "no@yo.com",
+        password: "password2",
+        role: "member"
+      )}
+
+      let!(:album1) {Album.create(
+        album: "test album 1",
+        art: "https://vignette.wikia.nocookie.net/selenagomez/images/4/46/Rare_Album_Cover.jpg/revision/latest?cb=20191212172712",
+        artist: "Selena Gomez",
+        year: 2020,
+        genre: "Pop",
+        user: user2
+        )}
+    it "should delete the desired album." do
+      prev_count = Album.count
+      Album.destroy(album1.id)
+      expect(Album.count).to eq(prev_count - 1)
+      end
+    end
+  end
 end
