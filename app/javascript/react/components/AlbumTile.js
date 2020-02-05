cff2caf9d8273c263d67b544a24b5b8f82111697
Zain Jakhar <zainjakhar@gmail.com>
2020-02-04 23:49:50 -0500

    Cleaned up spacing

--- app/javascript/react/components/AlbumTile.js
+++ app/javascript/react/components/AlbumTile.js
@@ -5,12 +5,19 @@ const AlbumTile = ({ albumData }) => {
   let { id, album, art, artist, year, genre } = albumData
 
   return(
-    <div className ="container tile-position art">
-      <Link to={`/albums/${id}`}><img src={art}/></Link>
-      <div className="overlay tile-info">
-        <span>{artist}</span> - <p>{album}</p>
+    <div className="tile-position">
+      <li className="container">
+        <Link to={`/albums/${id}`}>
+          <img src={art} />
+        </Link>
+      <div className="overlay">
+        <div className="tile-info">
+          <Link to={`/albums/${id}`}>
+            <span>{artist}</span> - <p>{album}</p>
+          </Link>
+        </div>
       </div>
-      <br />
+      </li>
     </div>
   )
 }
