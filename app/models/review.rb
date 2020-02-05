ca097b09b1c7c06720a5b37c18228c54a91f71f3
Evan Fernberg <evan@Evans-MacBook-Pro.local>
2020-02-05 15:51:10 -0500

    Deleted association

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :album
-  has_many :votes
 
   validates :rating, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 10 }
 end
