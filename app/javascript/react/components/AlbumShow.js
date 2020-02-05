629d99ae3975ec5805b9afe760556254f3cab666
vinhvinhvroom <vinh.doan13@gmail.com>
2020-02-05 14:48:37 -0500

    Added Average Rating to Album Show

--- app/javascript/react/components/AlbumShow.js
+++ app/javascript/react/components/AlbumShow.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 const AlbumShow = ({ albumObject }) => {
-  let { id, name, art, artist, year, genre, album } = albumObject
+  let { id, name, art, artist, year, genre, album, average } = albumObject
 
   return(
     <div>
       <h1>This is the album show page</h1>
       <h2>{album}</h2>
+      <h3>Average Rating: {average}</h3>
       <img src={art} />
-      <h3>{artist}</h3>
+      <h4>{artist}</h4>
       <h6>{year}</h6>
       <p>{genre}</p>
     </div>
