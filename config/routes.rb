7953661013aa9030faec636eb98f59b84ccada8b
Zain Jakhar <zainjakhar@gmail.com>
2020-02-06 15:12:18 -0500

    added route for new edit form

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ Rails.application.routes.draw do
   get "/albums", to: 'homes#index'
   get "/albums/new", to: 'homes#index'
   get "/albums/:id", to: 'homes#index'
+  get "/album/:id/edit", to: 'homes#index'
 
   namespace :api do
     namespace :v1 do
-      resources :albums, only: [:index, :show, :create, :destroy] do
+      resources :albums, only: [:index, :show, :create, :update, :destroy] do
         resources :reviews, only: [:show, :create]
       end
     end
