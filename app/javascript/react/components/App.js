6c10e3cb5b14932b3bb588fd7fd1fcb7a0bfbf29
Zain Jakhar <zainjakhar@gmail.com>
2020-02-06 15:15:54 -0500

    added route for edit album form

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import AlbumIndex from "./AlbumIndex"
 import NewAlbumFormContainer from "./NewAlbumFormContainer"
 import AlbumShowContainer from './AlbumShowContainer'
+import EditAlbumForm from './EditAlbumForm'
 
 export const App = (props) => {
   return (
@@ -11,6 +12,7 @@ export const App = (props) => {
         <Switch>
           <Route exact path="/" component={AlbumIndex} />
           <Route exact path="/albums" component={AlbumIndex} />
+          <Route exact path="/album/:id/edit" component={EditAlbumForm} />
           <Route exact path="/albums/new" component={NewAlbumFormContainer} />
           <Route exact path="/albums/:id" component={AlbumShowContainer} />
         </Switch>
