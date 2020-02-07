0aa85f3c2fdf8ad0980d87b8ddd0b78f491a233e
Zain Jakhar <zainjakhar@gmail.com>
2020-02-07 10:38:07 -0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,11 +10,33 @@ user3 = User.create!(email:"email3@email.com", role: "member", password:"123456"
 user4 = User.create!(email:"email4@email.com", role: "member", password:"123456")
 user5 = User.create!(email:"email5@email.com", role: "member", password:"123456")
 user6 = User.create!(email:"email6@email.com", role: "member", password:"123456")
+#
+Album.create(album:"Rare", art:"https://vignette.wikia.nocookie.net/selenagomez/images/4/46/Rare_Album_Cover.jpg/revision/latest?cb=20191212172712", artist: "Selena Gomez", year: 2020, genre: "pop", user:user3)
+#
+# Album.create(album:"Age of Unreason", art:"https://www.billboard.com/files/styles/900_wide/public/media/Bad-Religion-Age-of-Unreason-album-art-2019-billboard-1240.jpg", artist: "Bad Religion", year: 2019, genre: "Punk", user:user4)
+#
+# Album.create(album:"When We All Fall Asleep, Where Do We Go?", art:"https://www.billboard.com/files/styles/900_wide/public/media/Billie-Eilish-When-We-All-Fall-Asleep-Where-Do-We-Go_-album-art-2019-billboard-1240.jpg", artist: "Billie Eilish", year: 2019, genre: "Alt Pop", user:user5)
+#
+# Album.create(album:"A Big Day", art:"https://www.billboard.com/files/styles/900_wide/public/media/Chance-the-Rapper-The-Big-Day-album-art-2019-billboard-1240.jpg", artist: "Chance the Rapper", year: 2019, genre: "Hip Hop", user:user6)
 
 Album.create(album:"Rare", art:"https://vignette.wikia.nocookie.net/selenagomez/images/4/46/Rare_Album_Cover.jpg/revision/latest?cb=20191212172712", artist: "Selena Gomez", year: 2020, genre: "pop", user:user3)
 
-Album.create(album:"Age of Unreason", art:"https://www.billboard.com/files/styles/900_wide/public/media/Bad-Religion-Age-of-Unreason-album-art-2019-billboard-1240.jpg", artist: "Bad Religion", year: 2019, genre: "Punk", user:user4)
+Album.create(album:"The Sun Will Come Up", art:"https://www.billboard.com/files/styles/900_wide/public/media/Nina-Nesbitt-The-Sun-Will-Come-Up-the-Seasons-Will-Change-album-art-2019-billboard-1240.jpg", artist: "Nina Nesbitt", year: 2019, genre: "pop", user:user3)
 
-Album.create(album:"When We All Fall Asleep, Where Do We Go?", art:"https://www.billboard.com/files/styles/900_wide/public/media/Billie-Eilish-When-We-All-Fall-Asleep-Where-Do-We-Go_-album-art-2019-billboard-1240.jpg", artist: "Billie Eilish", year: 2019, genre: "Alt Pop", user:user5)
+Album.create(album:"Starry Night Over the Phone", art:"https://www.billboard.com/files/styles/1024x1024/public/media/Allday-Starry-Night-Over-the-Phone-album-art-2019-billboard-1240.jpg", artist: "Allday", year: 2019, genre: "Hip-Hop", user:user3)
 
-Album.create(album:"A Big Day", art:"https://www.billboard.com/files/styles/900_wide/public/media/Chance-the-Rapper-The-Big-Day-album-art-2019-billboard-1240.jpg", artist: "Chance the Rapper", year: 2019, genre: "Hip Hop", user:user6)
+Album.create(album:"Invasion of Privacy", art:"https://www.billboard.com/files/styles/1024x1024/public/media/cardi-b-invasion-of-privacy-album-art-2018-billboard-embed.jpg", artist: "Cardi-B", year: 2018, genre: "Alt Pop", user:user3)
+
+Album.create(album:"K.T.S.E", art:"https://www.billboard.com/files/styles/1024x1024/public/media/teyana-taylor-KTSE-album-art-2018-billboard-embed.jpg", artist: "Teyana Taylor", year: 2019, genre: "Hip Hop", user:user6)
+
+Album.create(album:"Nature", art:"https://www.billboard.com/files/styles/1024x1024/public/media/paul-kelly-nature-album-art-2018-billboard-embed.jpg", artist: "Paul Kelly", year: 2018, genre: "pop", user:user3)
+
+Album.create(album:"Cheese", art:"https://66.media.tumblr.com/06fcee47d7afc6cddf4e433a655bf4f9/tumblr_myzzyrQGDM1t479wao1_1280.jpg", artist: "Allday", year: 2010, genre: "DubStep", user:user3)
+
+Album.create(album:"Abbey Road", art:"https://www.udiscovermusic.com/wp-content/uploads/2015/10/AbbeyRoad.jpg", artist: "The Beatles", year: 1969, genre: "British Rock", user:user3)
+
+Album.create(album:"Back in Black", art:"https://www.udiscovermusic.com/wp-content/uploads/2015/10/theramones.jpg", artist: "Ramones", year: 1980, genre: "Rock n Roll", user:user3)
+
+Album.create(album:"Ramones", art:"https://www.udiscovermusic.com/wp-content/uploads/2015/10/Acdc_backinblack_cover.jpg", artist: "AC/DC", year: 1980, genre: "Rock n Roll", user:user3)
+
+Album.create(album:"The Dark Side of the Moon", art:"https://www.udiscovermusic.com/wp-content/uploads/2015/10/Pink_Floyd_-_Dark_Side_of_the_Moon.jpg", artist: "Pink Floyd", year: 1973, genre: "Rock", user:user3)
