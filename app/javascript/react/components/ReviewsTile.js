420df6edb7103010715d353e6d2eca446f6d8e17
Zain Jakhar <zainjakhar@gmail.com>
2020-02-07 11:18:37 -0500

    fixed test again

--- app/javascript/react/components/ReviewsTile.js
+++ app/javascript/react/components/ReviewsTile.js
@@ -7,10 +7,10 @@ const ReviewsTile = ({ reviewData }) => {
     <div className="reviews-box">
       <li>
         <img className="profile-photo" src={user.profile_photo.url}/>
-        <h4 className="review email">{email} </h4> <br/>
+        <h4 className="review email">{email}</h4> <br/>
         <h3 className="review rating">{rating}</h3>
-        <h4 className="review text">{review}</h4>
-        <div className="date">{review_date}</div>
+        <h5 className="review text">{review}</h5>
+        <p className="date">{review_date}</p>
       </li>
     </div>
   )
